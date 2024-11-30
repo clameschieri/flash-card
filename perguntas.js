b92f7895534f8da90d43af9d7e03989493281e6e
clameschieri <clarameschieri@gmail.com>
2024-11-29 22:31:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'Matemática',
+    'Como é a função de 1º grau?',
+    'f(x)=ax+b'
+)
+
+criaCartao(
+    'Português',
+    'Quais são os 4 porquês, e para que eles servem?',
+    'Por que - início de pergunta; Porque - resposta; Por quê - fim de pergunta; Porquê - substantivo.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a 3º lei de Newton?',
+    'Ação e Reação'
+)
+
+criaCartao(
+    'Química',
+    'Como ficou conhecido o modelo atômico de Thomson?',
+    'Pudim de passas'
+)
+
+criaCartao(
+    'Biologia',
+    'A qual reino pertencem as bactérias?',
+    'Reino Monera'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a data da Proclamação da República?',
+    '15 de novembro de 1889'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual continente fica o Egito?',
+    'Continente africano'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem disse a frase "Só sei que nada sei"?',
+    'Sócrates'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é gentrificação?',
+    'É um processo de transformação e supervalorização de uma determinada área'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou o quadro Guerra e Paz?',
+    'Candido Portinari'
+)
+
+criaCartao(
+    'Educação física',
+    'Qual é a altura da rede de vôlei feminino?',
+    '2,24m'
+)
